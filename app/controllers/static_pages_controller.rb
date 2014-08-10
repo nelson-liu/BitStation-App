1d46cd4a3934321185b4b61623e989f63ff7320a
Nelson Liu <nelsonliu@csail.mit.edu>
2014-08-09 21:51:39 -0700

    added homepage / rerouted for more pages
    also made minor css tweaks

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,14 @@
 class StaticPagesController < ApplicationController
   def homepage
   end
+  def about
+  end
+  def faq
+  end
+  def privacy
+  end
+  def security
+  end
+  def team
+  end
 end
