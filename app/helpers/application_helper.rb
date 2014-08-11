043012ad0b423fa69840f922f50c4b3022b70159
Jiahao Li <isundaylee.reg@gmail.com>
2014-08-11 21:28:57 +0800

    First page transaction history display.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def friendly_amount_from_money(amount, options = {})
+    friendly_amount(amount.amount, amount.currency, options)
+  end
+
+  def friendly_amount(amount, currency, options = {})
+    round = options[:round] || 2
+
+    case currency
+    when 'BTC'
+      (amount * 1000).round(round).to_s + ' mBTC'
+    when 'USD'
+      amount.round(round).to_s + ' USD'
+    end
+  end
 end
