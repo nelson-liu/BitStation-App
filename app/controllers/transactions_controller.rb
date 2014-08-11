4e61883ba736b98a741928f19bc84c24e15dc534
Jiahao Li <isundaylee.reg@gmail.com>
2014-08-11 13:11:11 +0800

    Adds multi-currency transaction. Temper

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,14 +2,23 @@ class TransactionsController < ApplicationController
   before_filter :ensure_signed_in, only: []
   before_filter :ensure_coinbase_account_linked, only: [:transact, :history, :exchange]
 
+  CURRENCIES = ["USD", "BTC"]
+
   MINIMUM_TRANSACTION_AMOUNT = 0.001
 
   def transact
     recipient = params[:kerberos]
     amount = params[:amount].to_f
+    currency = params[:currency]
     message = params[:message] || ''
 
     user = User.find_by(kerberos: recipient)
+
+    unless CURRENCIES.include?(currency)
+      flash[:error] = 'Invalid currency. '
+      redirect_to dashboard_url
+      return
+    end
 
     if user.nil?
       flash[:error] = 'The designated recipient hasn\'t joined BitStation yet. '
@@ -41,6 +50,7 @@ class TransactionsController < ApplicationController
       return
     end
 
+    amount /= (current_coinbase_client.spot_price(currency).to_d) unless currency == 'BTC'
     pt = PendingTransaction.create!({sender: current_user, recipient: user, amount: amount, message: message})
     redirect_to @oauth_client.auth_code.authorize_url(redirect_uri: coinbase_callback_uri + '?pending_action=transact&pending_action_id=' + pt.id.to_s, scope: 'send')
   end
