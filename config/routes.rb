3afba996a3b89f669da2d8a6316ffb39efc37f67
Jiahao Li <isundaylee.reg@gmail.com>
2014-08-13 02:18:25 +0800

    Sending part of money requests.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   get 'sessions/oauth', as: :oauth
 
   post 'transactions/transact', as: :transact
-  post 'transactions/request_bitcoin', as: :request_bitcoin
+  post 'transactions/request_money', as: :request_money
   get 'transactions/history'
   get 'transactions/exchange'
 
