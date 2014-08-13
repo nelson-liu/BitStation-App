d0fb9cf71eecd5fdd79932ddb3a2b2cf9d24b1f1
Jiahao Li <isundaylee.reg@gmail.com>
2014-08-14 00:56:40 +0800

    Changes various paths in unicorn.rb.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -27,14 +27,14 @@ working_directory APP_PATH # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen APP_PATH + "/tmp/sockets/.unicorn.sock", :backlog => 64
+listen APP_PATH + ".unicorn.sock", :backlog => 64
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
-pid APP_PATH + "/tmp/pids/unicorn.pid"
+pid APP_PATH + ".unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
