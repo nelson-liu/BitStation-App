1f54187a33009b62b9d2bb670ea27bca95c3154a
Jiahao Li <isundaylee.reg@gmail.com>
2014-08-23 10:05:25 -0400

    Fixes error in migration.

--- db/migrate/20140823035724_create_comments.rb
+++ db/migrate/20140823035724_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.belongs_to :user
-      t.belongs_to :associated_transaction, class_name
+      t.belongs_to :associated_transaction, class_name: 'Transaction'
       t.text :content
 
       t.timestamps
