884e20a95f50bab230d533fde2180f69caeec41c
Jiahao Li <isundaylee.reg@gmail.com>
2014-09-07 15:51:39 -0400

    Removes judges' backdoor.

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
   get 'users/access_qrcode', as: :access_qrcode
   get 'users/revoke_access_code', as: :revoke_access_code
 
-  get 'sessions/new', as: :sign_in
   get 'sessions/destroy', as: :sign_out
   match 'sessions/authenticate', via: [:get, :post]
   get 'sessions/fail'
