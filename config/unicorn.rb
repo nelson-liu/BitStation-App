995c8e5951f21727c5d7e17cb82a1265f5ba5cf2
Jiahao Li <isundaylee.reg@gmail.com>
2014-12-08 13:31:47 -0500

    Ooooooooooppppppppppppppppps.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -28,7 +28,7 @@ working_directory APP_PATH # available in 0.94.0+
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 # listen APP_PATH + "/.unicorn.sock", :backlog => 64
-listen "/tmp/unicorn.soundbook.sock"
+listen "/tmp/unicorn.bitstation.sock"
 listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
